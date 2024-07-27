2654a6f3095cf203ae6366a6707f50783bcab1d4
tkanzaki42 <88282411+tkanzaki42@users.noreply.github.com>
2024-07-27 15:59:53 +0900

    Revert "画像形式をjpgからpngに修正"
    This reverts commit ece3d8f585d0932a78d9b38646dc84add612e1a8.

--- scripts/sed.sh
+++ scripts/sed.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # CSVファイルのパス
-csv_file="../../42Tokyo-Tuning-2407/webapp/mysql/init/csv/users.csv"
+csv_file="パスを入力してください/42Tokyo-Tuning-2407/webapp/mysql/init/csv/users.csv"
 
 # 一時ファイルのパス
 temp_file=$(mktemp)
 
 # 0.pngから9.pngとdefault.pngをそれぞれ0_compressed.jpgから9_compressed.jpgとdefault_compressed.jpgに置換
-sed -e 's/0_compressed.jpg/0_compressed.png/g' \
-    -e 's/1_compressed.jpg/0_compressed.png/g' \
-    -e 's/2_compressed.jpg/0_compressed.png/g' \
-    -e 's/3_compressed.jpg/0_compressed.png/g' \
-    -e 's/4_compressed.jpg/0_compressed.png/g' \
-    -e 's/5_compressed.jpg/0_compressed.png/g' \
-    -e 's/6_compressed.jpg/0_compressed.png/g' \
-    -e 's/7_compressed.jpg/0_compressed.png/g' \
-    -e 's/8_compressed.jpg/0_compressed.png/g' \
-    -e 's/9_compressed.jpg/0_compressed.png/g' \
-    -e 's/default_compressed.jpg/default_compressed.png/g' \
+sed -e 's/0.png/0_compressed.jpg/g' \
+    -e 's/1.png/1_compressed.jpg/g' \
+    -e 's/2.png/2_compressed.jpg/g' \
+    -e 's/3.png/3_compressed.jpg/g' \
+    -e 's/4.png/4_compressed.jpg/g' \
+    -e 's/5.png/5_compressed.jpg/g' \
+    -e 's/6.png/6_compressed.jpg/g' \
+    -e 's/7.png/7_compressed.jpg/g' \
+    -e 's/8.png/8_compressed.jpg/g' \
+    -e 's/9.png/9_compressed.jpg/g' \
+    -e 's/default.png/default_compressed.jpg/g' \
     "$csv_file" > "$temp_file"
 
 # 一時ファイルを元のCSVファイルに上書き
