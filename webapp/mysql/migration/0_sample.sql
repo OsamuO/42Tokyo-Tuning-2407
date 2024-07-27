0ba0f3963fc9e79e28cfd556a33461c078aac37c
tkanzaki42 <88282411+tkanzaki42@users.noreply.github.com>
2024-07-27 18:36:15 +0900

    ログイン回りのインデックス追加

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,3 +1,7 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 ALTER TABLE users
     ALTER COLUMN profile_image SET DEFAULT 'default_compressed.png';
+
+CREATE INDEX idx_users_username ON users (username);
+
+CREATE INDEX idx_sessions_session_token ON sessions (session_token);
