96a41ebe62e1e3c52a3ca3980403c3d91067fff5
Taichi Kitagawa <takitaga@Taichinonotobukkukonpyuta.local>
2024-07-28 10:29:34 +0900

    password_hash

--- webapp/backend/src/utils.rs
+++ webapp/backend/src/utils.rs
@@ -1,13 +1,9 @@
-use argon2::{
-    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
-    Argon2,
-};
-use rand::Rng;
-
+use bcrypt::{hash, verify, DEFAULT_COST};
+use rand::{Rng, thread_rng};
 use crate::errors::AppError;
 
 pub fn generate_session_token() -> String {
-    let mut rng = rand::thread_rng();
+    let mut rng = thread_rng();
     let token: String = (0..30)
         .map(|_| {
             let idx = rng.gen_range(0..62);
@@ -19,27 +15,15 @@ pub fn generate_session_token() -> String {
 }
 
 pub fn hash_password(password: &str) -> Result<String, AppError> {
-    let password_bytes = password.as_bytes();
-    let salt = SaltString::generate(&mut OsRng);
-
-    // Argon2 with default params (Argon2id v19)
-    let argon2 = Argon2::default();
-
-    // Hash password to PHC string ($argon2id$v=19$...)
-    match argon2.hash_password(password_bytes, &salt) {
-        Ok(hashed_password_bytes) => Ok(hashed_password_bytes.to_string()),
+    match hash(password, DEFAULT_COST) {
+        Ok(hashed_password) => Ok(hashed_password),
         Err(_) => Err(AppError::InternalServerError),
     }
 }
 
 pub fn verify_password(hashed_password: &str, input_password: &str) -> Result<bool, AppError> {
-    let input_password_bytes = input_password.as_bytes();
-    let parsed_hash = match PasswordHash::new(hashed_password) {
-        Ok(hash) => hash,
-        Err(_) => return Err(AppError::InternalServerError),
-    };
-    match Argon2::default().verify_password(input_password_bytes, &parsed_hash) {
-        Ok(_) => Ok(true),
-        Err(_) => Ok(false),
+    match verify(input_password, hashed_password) {
+        Ok(result) => Ok(result),
+        Err(_) => Err(AppError::InternalServerError),
     }
 }
